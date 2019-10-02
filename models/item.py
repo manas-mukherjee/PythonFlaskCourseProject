7b0f633303f01e20314023c6f04c9760b6944ff0
Manas Mukherjee <mmanas19@gmail.com>
2019-10-02 01:59:50 -0700

    Added test files

--- models/item.py
+++ models/item.py
@@ -2,14 +2,14 @@ from db import db
 
 
 class ItemModel(db.Model):
-    __tablename__ = "items"
+    __tablename__ = 'items'
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80))
     price = db.Column(db.Float(precision=2))
 
-    store_id = db.Column(db.Integer, db.ForeignKey("stores.id"))
-    store = db.relationship("StoreModel")
+    store_id = db.Column(db.Integer, db.ForeignKey('stores.id'))
+    store = db.relationship('StoreModel')
 
     def __init__(self, name, price, store_id):
         self.name = name
@@ -17,20 +17,11 @@ class ItemModel(db.Model):
         self.store_id = store_id
 
     def json(self):
-        return {
-            "id": self.id,
-            "name": self.name,
-            "price": self.price,
-            "store_id": self.store_id,
-        }
+        return {'name': self.name, 'price': self.price}
 
     @classmethod
     def find_by_name(cls, name):
         return cls.query.filter_by(name=name).first()
-
-    @classmethod
-    def find_all(cls):
-        return cls.query.all()
 
     def save_to_db(self):
         db.session.add(self)
